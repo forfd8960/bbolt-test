4a9aaa887a1da8f808117e47e78199836d255661
forfd8960 <forfd8960@gmail.com>
2023-12-12 23:12:32 +0800

    Put and Get Key Value

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/forfd8960/bbolt-test
+
+go 1.21.3
+
+require (
+	go.etcd.io/bbolt v1.3.8 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	bolt "go.etcd.io/bbolt"
+)
+
+func main() {
+	// Open the my.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucket([]byte("MyBucket"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		fmt.Printf("Success create bucket: %+v\n", b)
+
+		b.Put([]byte(`answer`), []byte(`42`))
+		return nil
+	})
+
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("MyBucket"))
+		v := b.Get([]byte("answer"))
+		fmt.Printf("The answer is: %s\n", v)
+		return nil
+	})
+
+}
